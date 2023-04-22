dfcfb74f11a550b7bcfdf153e04d07ee860a1a53
rogerbage <correiodoroger@gmail.com>
2023-04-22 15:24:23 -0300

    Metaleg 0.1

--- gunicorn-cfg.py
+++ gunicorn-cfg.py
@@ -1,11 +1,7 @@
-# -*- encoding: utf-8 -*-
-"""
-Copyright (c) 2019 - present AppSeed.us
-"""
-
-bind = '0.0.0.0:5005'
+bind = '127.0.0.1:8000'
 workers = 1
 accesslog = '-'
 loglevel = 'debug'
 capture_output = True
 enable_stdio_inheritance = True
+logfile = "/home/rogerbage/dev/python/metaLeg/logs/gunicorn.log"
